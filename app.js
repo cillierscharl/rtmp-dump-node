9d047e9b78d2ceb2950009f6ee3d4f4e841129ea
Charl Cilliers <cilliers.charl@gmail.com>
2013-03-21 02:31:13 +0200

    Seperated RTMP Dump from the Twitch Implementation.

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var trd = require('./lib/node-twitch-rtmpdump');
+var rtmpDump = require('./lib/node-rtmpdump');
+var twitchRTMPDump = require('./lib/node-rtmpdump-twitch');
 
-var parser = new trd.Parser('bootsvii');
 
-parser.getStreamMetaData(function(){
-	parser.startRTMPDump();	
-});
+function twitchOutputToVlc(){
+	var output = new rtmpDump.VlcOutput('C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe'),
+		twitchDump = new twitchRTMPDump('cdewx','',output);
+}
 
+function twitchOutputToFile(){
+	var output = new rtmpDump.FileOutput('C:/testing.flv'),
+		twitchDump = new twitchRTMPDump('cdewx','',output);
+}
+
+function twitchOutputToFFMpeg(){
+	// Second Parameter are flags for FFMpeg. '' = DefaultSettings - c:a c:v
+	var output = new rtmpDump.FfmpegOutput('ffmpeg.exe','','rtmp://localhost/oflaDemo/live live=1'),
+		twitchDump = new twitchRTMPDump('cdewx','',output);
+}
+
+twitchOutputToFFMpeg();
